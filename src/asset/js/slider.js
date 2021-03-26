73acf009d396e21fc8ae124d1d2abe9479da97ba
nikawadori <nikawa.2161.t@gmail.com>
2021-03-26 22:02:21 +0900

    スライダー完成

--- src/asset/js/slider.js
+++ src/asset/js/slider.js
@@ -1,1 +1,20 @@
 // 画像スライダーの実装はこちらに記述してください
+document.addEventListener('DOMContentLoaded', function() {
+  //インスタス化
+  const hero = new HeroSlider();
+});
+
+class HeroSlider {
+  constructor() {
+    this.swiper = new Swiper('.swiper-container', {
+      loop: true,
+      effect: 'fade',
+      speed: 2000,
+      //○秒後にスライド
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+      },
+    });
+  }
+}
