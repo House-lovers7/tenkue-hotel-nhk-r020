45b6db2369d96699502224c7567c2a32895fde1b
nikawadori <nikawa.2161.t@gmail.com>
2021-04-02 16:12:52 +0900

    モーダル完成

--- src/asset/js/modal.js
+++ src/asset/js/modal.js
@@ -1,1 +1,20 @@
 // モーダルの実装はこちらに記述してください
+
+$(function () {
+  // モーダルのボタンをクリックした時
+  $('.js-modal-opener').on('click', function() {
+    // thisでクラスを指定
+    var i = $('.js-modal-opener').index(this); 
+    //.eqでindex番号の指定
+    $('.js-modal-element').eq(i).fadeIn(200);
+    //スクロールできないように
+    $('html,body').css('overflow', 'hidden');
+  });
+
+  //黒い背景色・クローズボタンをクリックした時
+  $('.js-modal-closer, .js-modal-cover').on('click', function() {
+    //removeAttr()で属性削除
+    $('html,body').removeAttr('style');
+    $('.js-modal-element').fadeOut(300);
+  });
+});
