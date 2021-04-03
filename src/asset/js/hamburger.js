63b103beec0dd262f565e4297dc3da5137acbd38
house-lovers7 <taisuke.gt@gmail.com>
2021-04-03 18:38:06 +0900

    作業進捗を確認のためのコミット

--- src/asset/js/hamburger.js
+++ src/asset/js/hamburger.js
@@ -1,1 +1,40 @@
 // ハンバーガーメニューの実装はこちらに記述してください
+
+
+$(function(){
+    $('.btn-trigger').on('click', function() {
+      $(this).toggleClass('active');
+      return false;
+    });
+  });
+
+  
+  // ページ遷移のJS
+
+  $(window).load(function() {
+    setTimeout(function(){
+      window.location.href = '/';
+    }, 3000);
+  });
+
+
+  // ページ遷移のJS
+
+
+  // ページを開くとき
+
+
+  $(function () {
+    $('.btn-trigger').on('click', function () {
+      $(this).toggleClass('active');
+      $("#js-buttonHamburger").toggleClass('active');
+    })
+  }) $(function () {
+    $('#global_nav a').on('click', function () {
+      $('#global_nav').toggleClass('active');
+      $("#js-buttonHamburger").toggleClass('active');
+    })
+  });
+
+
+  // ページを閉じる時
