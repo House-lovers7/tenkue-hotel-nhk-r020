ecb06462e84633cfea6c0ac516ed2f553f50ac44
house-lovers7 <taisuke.gt@gmail.com>
2021-04-04 14:26:33 +0900

    ハンバーガーメニューの3本ラインの動きの実装

--- src/asset/js/hamburger.js
+++ src/asset/js/hamburger.js
@@ -1,42 +1,14 @@
 // ハンバーガーメニューの実装はこちらに記述してください
 
-
-$(function(){
-    $('.btn-trigger').on('click', function() {
-      $(this).toggleClass('active');
-      return false;
-    });
-  });
-
-  
-  // ページ遷移のJS
-  $(window).load(function() {
-    setTimeout(function(){
-      window.location.href = '/';
-    }, 3000);
-  });
-
-
-  // ページを開くとき
-
-  $(function () {
-    $('.c-button').on('click', function () {
-      $(this).toggleClass('active');
-      $("#js-buttonHamburger").toggleClass('active');
-    })
-  }) $(function () {
-    $('#global_nav a').on('click', function () {
-      $('#global_nav').toggleClass('active');
-      $("#js-buttonHamburger").toggleClass('active');
-    })
-  });
-  // ページを閉じる時
-
-
-
   $(function(){
-    $('.btn-trigger').on('click', function() {
-      $(this).toggleClass('active');
-      return false;
+    $(".c-button").click(function() {
+      $("body").toggleClass('is-drawerActive');
+      $("#header-nav-wrap").slideToggle();
+
+      if ($(".c-button").attr('aria-expanded') == 'false') {
+        $(".c-button").attr('aria-expanded', true);
+      } else {
+          jQuery(".c-button").attr('aria-expanded', false);
+      }
     });
-  });
+});
